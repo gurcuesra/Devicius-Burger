be3c007adeae699f2bb031be15c52c8f191ef79e
gurcuesra <gurcuesra@gmail.com>
2021-01-03 12:56:49 -0800

    Convert App component to a class

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
+import React from 'react';
 import logo from './burger-logo.png';
 import './App.css';
 import Login from './components/Login';
 
-function App() {
-  return (
-    <div className="App">
-      <img src={logo} alt="Delicious Burger" id="logo" />
-      <hr />
-      <Login />
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state={}
+  }
+  render() {
+    return (
+      <div className="App">
+        <img src={logo} alt="Delicious Burger" id="logo" />
+        <hr />
+        <Login />
+      </div>
+    )
+  }
+  
 }
 
 export default App;
